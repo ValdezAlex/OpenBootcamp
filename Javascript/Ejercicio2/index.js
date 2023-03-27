72cf7be501c91f93b6a2431fcce3561446f77db4
Alex <no>
2023-03-27 11:31:34 -0300

    Add Ejercicio 2 Js

---
+++ Javascript/Ejercicio2/index.js
@@ -0,0 +1,18 @@
+let nombre = "Alex Valdez";
+let edad = 29;
+let desarrollador = true;
+const fecha_nacimiento = new Date(1994, 9, 18);
+let libro_favorito = {
+    titulo: "The Time Machine",
+    autor: "H.G.Wells",
+    fecha: new Date(1985,0,1),
+    url: "https://www.amazon.com/Time-Machine-H-G-Wells/dp/0553213512"
+};
+
+var lista = [
+    nombre,
+    edad,
+    desarrollador,
+    fecha_nacimiento,
+    libro_favorito
+];
